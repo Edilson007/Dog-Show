b99c4eeba3cc499ae7b6fb1667c1a42f469dd429
edilson007 <edilson-carlos@hotmail.com>
2021-10-21 13:26:34 -0300

    Implementando Accordion List em FAQ com JS

--- img/script.js
+++ img/script.js
@@ -21,23 +21,4 @@ if(tabMenu.length && tabContent.length) {
 
 initTab();
 
-
-function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
-    });
-  }
-}
-initAccordion();
+const accordionList = document.querySelectorAll('.js-accordion dt');
